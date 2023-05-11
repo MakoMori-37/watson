d8c6b64c317a2bb1ee5b17f97ab6992c783571f3
mako <oun_kup@hotmail.com>
2023-05-11 12:13:08 +0700

    import file btn

--- components/ManagementFace.jsx
+++ components/ManagementFace.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+
+const RenderFaceShop = () => {
+    return (
+        <div>
+            dsds
+        </div>
+    )
+}
 
 const ManagementFace = () => {
   return (
-    <div>ManagementFace</div>
-  )
-}
+    <div className="bg__default w-full h-screen relative">
+      <img
+        src="/image/logo.webp"
+        alt="logo"
+        className="w-auto h-auto absolute object-contain top-5 right-2 "
+      />
+      <RenderFaceShop/>
+    </div>
+  );
+};
 
-export default ManagementFace
+export default ManagementFace;
