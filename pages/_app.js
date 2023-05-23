02d7b71521328d769a691a8f0e2c426ced23afb7
mako <oun_kup@hotmail.com>
2023-05-23 11:50:12 +0700

    update i con web

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Head from "next/head";
+const HOST = process.env.HOST;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Head>
+        <title>Watson</title>
+        <link rel="icon" href={`${HOST}/image/logo_icon.ico`} />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
