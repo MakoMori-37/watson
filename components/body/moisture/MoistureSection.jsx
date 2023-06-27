bbd1e6c0af5acda4e9ed867c980066722cba9ee6
MakoMori-37 <oun_kup@hotmail.com>
2023-06-27 11:56:06 +0700

    set function on back to moisture

--- components/body/moisture/MoistureSection.jsx
+++ components/body/moisture/MoistureSection.jsx
@@ -25,9 +25,20 @@ const RenderStep1 = ({ onNext = () => {} }) => {
 
 const MoistureSection = ({ onReset = () => {} }) => {
   const [step, setStep] = useState(1);
+  const [reverseMode, setReverseMode] = useState(false);
 
   const incrementStep = () => {
     setStep(step + 1);
+  };
+
+  const previousStep = () => {
+    if (step === 2) {
+      setReverseMode(false);
+      setStep(1);
+    } else {
+      setReverseMode(true);
+      setStep(2);
+    }
   };
 
   const RenderStep = () => {
@@ -35,15 +46,37 @@ const MoistureSection = ({ onReset = () => {} }) => {
       case 1:
         return <RenderStep1 onNext={incrementStep} />;
       case 2:
-        return <Vaseline onUpdateSection={incrementStep} isSection3={true} />;
+        return (
+          <Vaseline
+            isSection3={true}
+            onBack={previousStep}
+            reverseMode={reverseMode}
+            onUpdateSection={incrementStep}
+          />
+        );
       case 3:
-        return <Citra onReset={onReset} isSection3={true} />;
+        return (
+          <Citra onBack={previousStep} onReset={onReset} isSection3={true} />
+        );
       default:
-        return <div></div>;
+        return <></>;
     }
   };
 
-  return <div className="z-20 w-full h-screen">{RenderStep()}</div>;
+  return (
+    <div className="z-20 w-full h-screen">
+      {RenderStep()}
+
+      {step == 1 ? (
+        <img
+          alt="content"
+          onClick={onReset}
+          src={`${HOST}/image/btn_back.webp`}
+          className="w-20 absolute bottom-6 right-6 "
+        />
+      ) : null}
+    </div>
+  );
 };
 
 export default MoistureSection;
